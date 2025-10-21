44ba66aa175d987b06421a17f85ac8bce38387ee
Sika Kase3 <sikakase39@gmail.com>
2025-10-21 05:37:33 +0000

    Try fixing this error: `Runtime Error: Error: Attempted to call initiali

--- src/lib/jobs.ts
+++ src/lib/jobs.ts
@@ -1,12 +1,21 @@
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
-import { initializeFirebase } from '@/firebase';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { firebaseConfig } from '@/firebase/config';
 import type { Job } from './data';
+
+function initializeServerFirebase() {
+  if (!getApps().length) {
+    return initializeApp(firebaseConfig);
+  }
+  return getApp();
+}
 
 // This function must be called on the server, e.g. in Server Components.
 export async function getJob(id: string): Promise<Job | null> {
   // We need to initialize a new instance of firebase on the server.
   // Note that this is a different instance than the one on the client.
-  const { firestore } = initializeFirebase();
+  const firebaseApp = initializeServerFirebase();
+  const firestore = getFirestore(firebaseApp);
   
   const jobRef = doc(firestore, 'jobListings', id);
   const jobSnap = await getDoc(jobRef);
