b9ddc10b7184bbf62aa9dc837a9345c43a2e560c
Sika Kase3 <sikakase39@gmail.com>
2025-10-23 21:56:48 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -27,6 +27,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Loader2 } from "lucide-react";
 import React from "react";
