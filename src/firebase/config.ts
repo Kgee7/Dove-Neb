6e04856dba2b7249c5ea8710197095940cfbf689
Sika Kase3 <sikakase39@gmail.com>
2025-11-10 22:08:12 +0000

    new

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7235955659-7c316",
-  "appId": "1:652128123659:web:b9f78cbe5d7b52f5b7800e",
-  "apiKey": "AIzaSyDgsRLUvhyldbLVvl0srYI8X3WriAP2Nb0",
-  "authDomain": "studio-7235955659-7c316.firebaseapp.com",
-  "storageBucket": "studio-7235955659-7c316.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "652128123659"
+  apiKey: "AIzaSyDgSgNF_VK2wrgDkUO0hoK17POdT-jQxfM",
+  authDomain: "deve-neb.firebaseapp.com",
+  projectId: "deve-neb",
+  storageBucket: "deve-neb.appspot.com",
+  messagingSenderId: "1073585781619",
+  appId: "1:1073585781619:web:a5c10c55df959dc75fe306"
 };
