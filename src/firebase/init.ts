0cfdec3b65be17752003bb0a808fa85004d28622
Sika Kase3 <sikakase39@gmail.com>
2025-11-10 20:38:00 +0000

    fire

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -2,18 +2,21 @@
 import { getApp, getApps, initializeApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
+import { getStorage, FirebaseStorage } from 'firebase/storage';
 import { firebaseConfig } from './config';
 
 interface FirebaseServices {
   firebaseApp: FirebaseApp;
   auth: Auth;
   firestore: Firestore;
+  storage: FirebaseStorage;
 }
 
 export function initializeFirebase(): FirebaseServices {
   const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
   const auth = getAuth(app);
   const firestore = getFirestore(app);
+  const storage = getStorage(app);
 
-  return { firebaseApp: app, auth, firestore };
+  return { firebaseApp: app, auth, firestore, storage };
 }
