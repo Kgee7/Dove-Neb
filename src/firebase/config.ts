1ffe9d2d81f3fe527784abb002d5464fdb8898c1
Sika Kase3 <sikakase39@gmail.com>
2025-11-11 18:33:50 +0000

    con

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA4t-98e_VEsE_vWNo_YGCtf_Gv-d6oT7s",
-  authDomain: "dev-neb-2-e14b0.firebaseapp.com",
-  projectId: "dev-neb-2-e14b0",
-  storageBucket: "dev-neb-2-e14b0.appspot.com",
-  messagingSenderId: "365692695420",
-  appId: "1:365692695420:web:4a25c1106a77d4b4a1b023"
+  apiKey: "AIzaSyDgsRLUvhyldbLVvl0srYI8X3WriAP2Nb0",
+  authDomain: "studio-7235955659-7c316.firebaseapp.com",
+  projectId: "studio-7235955659-7c316",
+  storageBucket: "studio-7235955659-7c316.firebasestorage.app",
+  messagingSenderId: "652128123659",
+  appId: "1:652128123659:web:adb418df8a014d6fb7800e",
+  measurementId: "G-DPNHP20CWK"
 };
