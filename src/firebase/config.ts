06ab1e0eca25073c62d0d5ed5de88b783461e1d1
Sika Kase3 <sikakase39@gmail.com>
2025-11-11 18:17:49 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDgSgNF_VK2wrgDkUO0hoK17POdT-jQxfM",
-  authDomain: "deve-neb.firebaseapp.com",
-  projectId: "deve-neb",
-  storageBucket: "deve-neb.appspot.com",
-  messagingSenderId: "1073585781619",
-  appId: "1:1073585781619:web:a5c10c55df959dc75fe306"
+  apiKey: "AIzaSyC2kptpZ5-2s5gQW_uGjQT0mYOGw7VdE9Y",
+  authDomain: "dev-neb.firebaseapp.com",
+  projectId: "dev-neb",
+  storageBucket: "dev-neb.appspot.com",
+  messagingSenderId: "365692695420",
+  appId: "1:365692695420:web:1e6a0d2d6c1c87513364f9"
 };
