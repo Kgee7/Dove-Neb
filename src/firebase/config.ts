e84d1accf4b389a2281411e97a377115aefd4fc7
Sika Kase3 <sikakase39@gmail.com>
2026-02-08 02:25:41 +0000

    When uploading photo loads unstoppable without uploading the image

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDgsRLUvhyldbLVvl0srYI8X3WriAP2Nb0",
   authDomain: "studio-7235955659-7c316.firebaseapp.com",
   projectId: "studio-7235955659-7c316",
-  storageBucket: "studio-7235955659-7c316.firebasestorage.app",
+  storageBucket: "studio-7235955659-7c316.appspot.com",
   messagingSenderId: "652128123659",
   appId: "1:652128123659:web:adb418df8a014d6fb7800e",
   measurementId: "G-DPNHP20CWK"
