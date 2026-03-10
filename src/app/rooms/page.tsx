2f0a3deca59be86a9f6451b5ecb82c53b95b4ef3
Sika Kase3 <sikakase39@gmail.com>
2026-03-10 01:39:17 +0000

    nebo

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,10 +1,11 @@
 
 import { Suspense } from 'react';
 import Rooms from './Rooms';
+import { Loader2 } from 'lucide-react';
 
 export default function RoomsListingPage() {
   return (
-    <Suspense fallback={<div className="flex justify-center items-center h-64">Loading...</div>}>
+    <Suspense fallback={<div className="flex justify-center items-center h-screen"><Loader2 className="h-12 w-12 animate-spin" /></div>}>
       <Rooms />
     </Suspense>
   );
